106f2ff1462bec7f40a0ec8c60867d547c76bd21
JaayTeeee <leejertao@gmail.com>
2023-08-24 22:52:19 +0800

    async message

--- app/chat/client/ChatProgram.tsx
+++ app/chat/client/ChatProgram.tsx
@@ -1,166 +1,203 @@
-"use client";
 import UserIcon from "@/public/component/icons/icons8-user-100.png";
 import { Text } from "@/public/styles/chakra";
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import ScrollToBottom from "react-scroll-to-bottom";
+import React, { useEffect, useRef, useState } from "react";
 import "../../globals.css";
 import ChatInterface from "./ChatInterface";
 
 interface Message {
-  room: number;
   author: string;
   message: string;
   time: string;
 }
 
-const ChatProgram = ({
-  username,
-  room,
-}: {
-  username: string;
-  room: number;
-}) => {
+const ChatProgram = ({ username }: { username: string }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState<Message[]>([]);
   const [connectionStatus, setConnectionStatus] = useState<
     "connecting" | "connected" | "failed" | "closed"
   >("connecting"); // Initialize with 'connecting'
 
-  const socket = new WebSocket("ws://localhost:4000");
+  const socket = useRef<WebSocket | null>(null);
 
   const sendMessage = () => {
     if (currentMessage !== "") {
       const messageData = {
-        room: room,
         author: username,
         message: currentMessage,
         time: `${new Date().getHours()}:${new Date().getMinutes()}`,
       };
 
-      socket.send(JSON.stringify(messageData));
-
-      setMessageList((list) => [...list, messageData]);
-
-      setCurrentMessage("");
+      if (socket.current) {
+        socket.current.send(JSON.stringify(messageData));
+        setMessageList((list) => [...list, messageData]);
+        setCurrentMessage("");
+      }
     }
   };
 
   useEffect(() => {
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      if (data.author !== username) {
-        setMessageList((list) => [...list, data]);
+    socket.current = new WebSocket("ws://localhost:4000");
+
+    socket.current.onmessage = (event: MessageEvent) => {
+      const responseData = event.data;
+      let data: any;
+
+      if (responseData instanceof Blob) {
+        const reader = new FileReader();
+
+        reader.onload = () => {
+          const parsedData = reader.result as string;
+
+          try {
+            data = JSON.parse(parsedData);
+          } catch (error) {
+            console.error("Failed to parse response data:", error);
+            return;
+          }
+
+          if (data.author !== username) {
+            setMessageList((list) => [...list, data]);
+          }
+        };
+
+        reader.readAsText(responseData);
+      } else {
+        try {
+          data = JSON.parse(responseData);
+        } catch (error) {
+          console.error("Failed to parse response data:", error);
+          return;
+        }
+
+        if (data.author !== username) {
+          setMessageList((list) => [...list, data]);
+        }
       }
     };
 
-    socket.onopen = () => {
+    socket.current.onopen = () => {
       setConnectionStatus("connected");
     };
 
-    socket.onerror = () => {
+    socket.current.onerror = () => {
       setConnectionStatus("failed");
     };
 
-    socket.onclose = () => {
+    socket.current.onclose = () => {
       setConnectionStatus("closed");
     };
 
     return () => {
-      socket.close();
+      if (socket.current) {
+        socket.current.close();
+      }
     };
-  }, []);
+  }, [username]);
 
-  console.log(connectionStatus);
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (messageList.length) {
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "end",
+      });
+    }
+  }, [messageList]);
 
   return (
     <ChatInterface>
       <div className="chat-interface-container" style={{ textAlign: "center" }}>
-        {/* Display connection status */}
-        {connectionStatus === "connected" ? (
-          <div className="success-message">
-            Connected with user successfully...Enjoy the chat!
-          </div>
-        ) : connectionStatus === "failed" ? (
-          <div className="error-message">Failed to connect to the server</div>
-        ) : connectionStatus === "closed" ? (
-          <div className="close-message">User has left the chat</div>
-        ) : (
-          <div className="connecting-message">Connecting with user...</div>
-        )}
-
         <div className="chat-body">
-          <ScrollToBottom className="message-container">
-            {messageList.map((messageContent) => {
-              return (
-                <>
-                  {username === messageContent.author ? (
-                    <div className="flex flex-row mr-[5rem] mt-[1rem] mb-[1rem]">
-                      <div className="bubbleChat">
-                        <Text
-                          style={{
-                            color: "white",
-                            display: "flex",
-                            alignItems: "right",
-                            justifyContent: "left",
-                          }}
-                        >
-                          {messageContent.message}
-                        </Text>
-                      </div>
-                      <Image
-                        src={UserIcon}
-                        alt="user-icon"
+          <div className="mt-[2rem]" style={{ color: "grey", opacity: 0.7 }}>
+            {connectionStatus === "connected" ? (
+              <div className="success-message">
+                Connected with user successfully... Enjoy the chat!
+              </div>
+            ) : connectionStatus === "failed" ? (
+              <div className="error-message">
+                Failed to connect to the server
+              </div>
+            ) : connectionStatus === "closed" ? (
+              <div className="close-message">User has left the chat</div>
+            ) : (
+              <div className="connecting-message">Connecting with user...</div>
+            )}
+          </div>
+          {messageList.map((messageContent, index) => {
+            return (
+              <React.Fragment key={index}>
+                {username === messageContent.author ? (
+                  <div className="flex flex-row mr-[5rem] mt-[1rem] mb-[1rem]">
+                    <div className="bubbleChat">
+                      <Text
                         style={{
-                          width: "45px",
-                          height: "45px",
-                          borderRadius: "100px",
-                          backgroundColor: "black",
-                          alignContent: "center",
-                          marginLeft: "20px",
-                        }}
-                      />
-                    </div>
-                  ) : (
-                    <div className="flex flex-row ml-[3rem] mt-[1rem]">
-                      <Image
-                        src={UserIcon}
-                        alt="user-icon"
-                        style={{
-                          width: "45px",
-                          height: "45px",
-                          borderRadius: "100px",
-                          backgroundColor: "grey",
-                          alignContent: "center",
-                          marginLeft: "20px",
-                        }}
-                      />
-                      <div
-                        className="bubbleChat"
-                        style={{
-                          backgroundColor: "grey",
-                          marginLeft: "1rem", // Add margin to create space between the image and the bubble chat
-                          display: "flex", // Set flex display to enable flex properties
-                          alignItems: "center", // Center the content vertically
-                          padding: "0.5rem", // Add padding to the bubble chat container
+                          color: "white",
+                          display: "flex",
+                          alignItems: "right",
+                          justifyContent: "left",
                         }}
                       >
-                        <Text
-                          style={{
-                            color: "white",
-                            display: "flex",
-                            alignItems: "center", // Center the text vertically within the bubble chat
-                          }}
-                        >
-                          {messageContent.message}
-                        </Text>
-                      </div>
+                        {messageContent.message}
+                      </Text>
                     </div>
-                  )}
-                </>
-              );
-            })}
-          </ScrollToBottom>
+                    <Image
+                      src={UserIcon}
+                      alt="user-icon"
+                      style={{
+                        width: "45px",
+                        height: "45px",
+                        borderRadius: "100px",
+                        backgroundColor: "black",
+                        alignContent: "center",
+                        marginLeft: "20px",
+                      }}
+                      width={45}
+                      height={45}
+                    />
+                  </div>
+                ) : (
+                  <div className="flex flex-row ml-[3rem] mt-[1rem]">
+                    <Image
+                      src={UserIcon}
+                      alt="user-icon"
+                      style={{
+                        width: "45px",
+                        height: "45px",
+                        borderRadius: "100px",
+                        backgroundColor: "grey",
+                        alignContent: "center",
+                        marginLeft: "20px",
+                      }}
+                      width={45}
+                      height={45}
+                    />
+                    <div
+                      className="bubbleChat"
+                      style={{
+                        backgroundColor: "grey",
+                        marginLeft: "1rem",
+                        display: "flex",
+                        alignItems: "center",
+                        padding: "0.5rem",
+                      }}
+                    >
+                      <Text
+                        style={{
+                          color: "white",
+                          display: "flex",
+                          alignItems: "center",
+                        }}
+                      >
+                        {messageContent.message}
+                      </Text>
+                    </div>
+                  </div>
+                )}
+              </React.Fragment>
+            );
+          })}
+          <div ref={ref} />
         </div>
         <div className="chat-footer flex flex-row justify-center">
           <input
@@ -170,7 +207,7 @@ const ChatProgram = ({
               height: "44px",
               backgroundColor: "#FFFFFF",
               textIndent: "10px",
-              marginTop: "10px",
+              marginTop: "20px",
               outline: "none",
               marginBottom: "10px",
               borderRadius: "8px",
--- app/chat/server/server.js
+++ app/chat/server/server.js
@@ -7,20 +7,86 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+// Store the connected users
+const connectedUsers = new Set();
+
 wss.on("connection", function connection(ws) {
   const address = ws._socket.remoteAddress;
   console.log("Connection received: ", address);
 
-  ws.on("message", wss.broadcast);
-});
+  ws.on("message", function incoming(message) {
+    const data = JSON.parse(message);
+    if (data.type === "connect") {
+      // Store the username of the connected user
+      connectedUsers.add(data.username);
 
-wss.broadcast = function broadcastMsg(sender, msg) {
-  wss.clients.forEach(function each(client) {
-    if (client !== sender) {
-      client.send(msg);
+      // Notify the other user about the connection
+      const otherUser = getOtherUser(data.username);
+      if (otherUser) {
+        sendConnectionStatus(otherUser, "connected");
+      }
+    } else {
+      // Broadcast the message to all connected clients
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(message);
+        }
+      });
     }
   });
-};
+
+  ws.on("close", function close() {
+    const username = getUsernameBySocket(ws);
+    if (username) {
+      // Remove the disconnected user from the set
+      connectedUsers.delete(username);
+
+      // Notify the other user about the disconnection
+      const otherUser = getOtherUser(username);
+      if (otherUser) {
+        sendConnectionStatus(otherUser, "closed");
+      }
+    }
+  });
+});
+
+function getUsernameBySocket(socket) {
+  // Find the username based on the socket object
+  for (const client of wss.clients) {
+    if (client === socket) {
+      return client.username;
+    }
+  }
+  return null;
+}
+
+function getOtherUser(username) {
+  // Find the other user's username
+  for (const user of connectedUsers) {
+    if (user !== username) {
+      return user;
+    }
+  }
+  return null;
+}
+
+function sendConnectionStatus(username, status) {
+  // Send connection status message to the specified user
+  const client = getClientByUsername(username);
+  if (client) {
+    client.send(JSON.stringify({ type: "connectionStatus", username, status }));
+  }
+}
+
+function getClientByUsername(username) {
+  // Find the client WebSocket object based on the username
+  for (const client of wss.clients) {
+    if (client.username === username) {
+      return client;
+    }
+  }
+  return null;
+}
 
 server.listen(port, function () {
   console.log(`Server is listening on ${port}!`);
